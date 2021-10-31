7731307df715fc5b803937def8333e44a98baaf8
James Serra <Jamesserra4@gmail.com>
2021-10-31 13:51:05 -0400

    shuffle deck potentially

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function createDeck() {
     }
     return deck;
 }
+
+function shuffleDeck(deck) {
+    for(let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
